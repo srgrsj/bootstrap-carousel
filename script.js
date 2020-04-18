c726abbb5f62fab592712f291729be115bbaa501
towar4221 <towar4221@gmail.com>
2020-04-18 16:34:54 +0700

    update

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-
-
-$("#prev").click(function (e) { 
-    $("#slideShow").carousel('prev')
-    
+$("#prev").click(function(e){
+    $("#slideShow").carousel('prev');
 });
 
-$("#next").click(function (e) { 
-    $("#slideShow").carousel('next')
-    
+
+
+$("#next").click(function(e){
+    $("#slideShow").carousel('next');
 });
 
-$("#play").click(function (e) { 
-    $("#slideShow").carousel('pause')
-    
-});
 
+$('#play').click(function() {
+    let controls = $(this);
+    if (controls.hasClass('pause')) {
+        controls.text('▶').removeClass('pause').toggleClass('play');
+        $('#slideShow').carousel('pause');
+    } else {
+        controls.text('⏸').removeClass('play').toggleClass('pause');
+        $('#slideShow').carousel('cycle');
+    }
+});
 
