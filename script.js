1c4b613a3236fab9dbbf8abc783307b30706755e
towar4221 <towar4221@gmail.com>
2020-04-18 14:28:44 +0700

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+$("#prev").click(function (e) { 
+    $("#slideShow").carousel('prev')
+    
+});
+
+$("#next").click(function (e) { 
+    $("#slideShow").carousel('next')
+    
+});
+
+$("#play").click(function (e) { 
+    $("#slideShow").carousel('pause')
+    
+});
+
+
